62a9feb74889b8a8874d25efeaf6bbecefe75d27
liudas-stonys <liudas.stonys@gmail.com>
2019-07-10 19:49:37 +0300

    updated form

--- scripts/script.js
+++ scripts/script.js
@@ -14,6 +14,35 @@ $(document).ready(function() {
 	});
 
 	$('#submit').on('click', function(e) {
-	    e.preventDefault();
+		// e.preventDefault();
+		// sendEmail();
 	})
 });
+
+// document.getElementById("contactForm").onsubmit = () => {
+// 	alert(document.getElementById("message").value);
+// }
+
+sendEmail = () => {
+	Email.send({
+		Host : "smtp.elasticemail.com",
+		Username : "liudas.stonys@gmail.com",
+		Password : "504270ff-8cc6-42ed-953f-b155a81ecd7d",
+		To : "liudas.stonys@gmail.com",
+		From : "liudas.stonys@gmail.com",
+		Subject : "liudas-stonys.github.io | " + document.getElementById("name").value + " | " + document.getElementById("email").value + " | " + document.getElementById("phone").value,
+		Body : document.getElementById("message").value
+	}).then(
+		message => alert(message)
+	);
+}
+
+
+// Google API key
+// key=AIzaSyBH5Z3b_LOFktStwGRaUruZZDD_tFzv2Yc
+
+// Client ID
+// 318020525394-qmdcbpn2g6m0tqo6sb118m5kf02hvi31.apps.googleusercontent.com
+
+// Client secret
+// ipU8D8nA-wkuwZKk1EfgChNk
